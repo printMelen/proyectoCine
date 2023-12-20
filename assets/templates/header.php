db6617af9d9c382f3f29f388c163d2b17a78013f
printMelen <alvaro.redondofgb@gmail.com>
2023-12-21 00:33:46 +0100

    Header arreglado

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -1,15 +1,18 @@
 <header class="container max-w-screen-xl mx-auto pt-[14px]">
 <!-- minmax(480px,819px) -->
-    <nav class="grid grid-cols-[180px_891px_121px] items-center justify-between">
-        <a href="#">
-            <img src="assets/images/playOn.svg" alt="" srcset="">
-        </a>
-        <ul class="text-white flex gap-[50px]">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">Series</a></li>
-            <li><a href="#">Top</a></li>
-        </ul>
+<!-- grid-cols-[180px_891px_121px] -->
+    <nav class="grid grid-cols-2 items-center justify-between text-18.75 font-400">
+        <div class="flex items-center gap-[54px]">
+            <a href="#">
+                <img src="assets/images/playOn.svg" alt="" srcset="">
+            </a>
+            <ul class="text-white flex gap-[50px]">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">Series</a></li>
+                <li><a href="#">Top</a></li>
+            </ul>
+        </div>
         <div class="flex items-center gap-[25px] justify-end">
             <a href="#" >
                 <img src="assets/images/lupa.svg" alt="" srcset="">
