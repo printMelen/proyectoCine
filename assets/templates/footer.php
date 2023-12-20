c66cfd5120c30861d0432fe0d6f07150edbb3c21
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2023-12-20 21:02:02 +0100

    Parte del footer

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -1,4 +1,21 @@
-<footer>
-    
+<footer class="container max-w-screen-xl mx-auto">
+        <ul>
+            <li><a href=""><img src="assets/images/iconoir_instagram.svg" alt="" srcset=""></a></li>
+            <li><a href=""><img src="assets/images/ic_baseline-facebook.svg" alt="" srcset=""></a></li>
+            <li><a href=""><img src="assets/images/mdi_twitter.svg" alt="" srcset=""></a></li>
+            <li><a href=""><img src="assets/images/mingcute_telegram-fill.svg" alt="" srcset=""></a></li>
+        </ul>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Info</a></li>
+            <li><a href="">Support</a></li>
+            <li><a href="">Contact us</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Account</a></li>
+            <li><a href="">Help Center</a></li>
+        </ul>
+    </nav>
 </footer>
 
