e7db88dea647a245e7f850452f07b9f15a28a186
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2023-12-21 17:30:02 +0100

    Añadido imagen de butacas y cambio en el register

--- assets/templates/register.php
+++ assets/templates/register.php
@@ -16,7 +16,7 @@
                     <input type="email" placeholder="Email" name="" id="" class="w-[100%] mb-[14px] h-[50px] rounded-[10px] bg-backInputs pl-[21px] backdrop-blur-[10px]">
                     <input type="password" placeholder="Password" name="" id="" class="w-[100%] mb-[14px] h-[50px] rounded-[10px] bg-backInputs pl-[21px] backdrop-blur-[10px]">
                     <input type="password" placeholder="Confirmar password" name="" id="" class="w-[100%] mb-[10px] h-[50px] rounded-[10px] bg-backInputs pl-[21px] backdrop-blur-[10px]">
-                    <label class="text-12 flex justify-end mb-[10px] items-center"><input type="checkbox" id="cbox1" value="acepta" class=" appearance-none w-3 h-3 rounded-sm bg-login  checked:bg-pink mr-[2px]">He leido y acepto la &nbsp<a href="#" class="underline"> política de privacidad</a></label>
+                    <label class="text-12 flex justify-end mb-[10px] items-center"><input type="checkbox" id="cbox1" value="acepta" class=" appearance-none w-3 h-3 rounded-sm bg-login backdrop-blur-[10px] checked:bg-pink mr-[2px]">He leido y acepto la &nbsp<a href="#" class="underline"> política de privacidad</a></label>
                     <input type="submit" value="Registrarme" class="bg-pink backdrop-blur-[10px] w-[100%] h-[50px] font-600 text-22 rounded-[10px]">
                 </form>
             </div>
