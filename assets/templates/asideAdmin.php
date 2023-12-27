2abd4e79724b5d9039612438a92d48382349a19f
printMelen <alvaro.redondofgb@gmail.com>
2023-12-27 21:33:06 +0100

    Formulario Peli acabado

--- assets/templates/asideAdmin.php
+++ assets/templates/asideAdmin.php
@@ -3,7 +3,7 @@
           <nav class="text-white">
                <ul class="w-[200px]">
                     <li><a href="#" class="flex items-center gap-[10px] pb-[20px]"><img src="../images/MaterialSymbolsAddCircleOutline.svg" class="w-[30px]" alt="" srcset="">Añadir película</a></li>
-                    <li><a href="#" class="flex items-center gap-[10px] pb-[20px]"><img src="../images/MingcuteUserAdd2Fill.svg" class="w-[30px]" alt="" srcset="">Añadir usuario</a></li>
+                    <li><a href="#" class="flex items-center gap-[10px] pb-[20px]"><img src="../images/MingcuteUserAdd2Fill.svg" class="w-[30px]" alt="" srcset="">Añadir actor</a></li>
                     <li><a href="#" class="flex items-center gap-[10px] pb-[20px]"><img src="../images/FluentMoviesAndTv16Filled.svg" class="w-[30px]" alt="" srcset="">Mostrar Peliculas</a></li>
                </ul>
           </nav>  
--- assets/templates/formularioPeli.php
+++ assets/templates/formularioPeli.php
@@ -1,30 +1,39 @@
-<div class="container mx-auto py-8">
+<div class="container mx-auto py-8 text-18.75 font-600">
     <h1 class="text-2xl font-bold mb-6 text-center">Añadir una película</h1>
     <form class="w-full max-w-sm mx-auto bg-white p-8 rounded-md shadow-md">
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Nombre:</label>
+        <label class="block text-gray-700 text-15 mb-2" for="name">Nombre:</label>
         <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
           type="text" id="name" name="name" placeholder="Shrek">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Argumento:</label>
+        <label class="block text-gray-700 text-15  mb-2" for="argumento">Argumento:</label>
         <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-          type="email" id="email" name="email" placeholder="En una misión para rescatar a una hermosa princesa de las garras de un dragón">
+          type="text" id="argumento" name="argumento" placeholder="En una misión para rescatar a una hermosa princesa de las garras de un dragón">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Carátula:</label>
-        <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-          type="password" id="password" name="password" placeholder="********">
+        <label class="block text-gray-700 text-15  mb-2" for="caratula">Carátula:</label>
+        <input class="w-full px-3 py-2 border border-gray-300 text-15 rounded-md focus:outline-none focus:border-indigo-500"
+          type="file" id="caratula" name="caratula">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="confirm-password">Edad mínima:</label>
+        <label class="block text-gray-700 text-15  mb-2" for="edadMinima">Edad mínima:</label>
         <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-          type="number" id="confirm-password" name="confirm-password" placeholder="7">
+          type="number" id="edadMinima" name="edadMinima" placeholder="7">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="confirm-password">Género:</label>
+        <label class="block text-gray-700 text-15  mb-2" for="genero">Género:</label>
         <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-          type="text" id="confirm-password" name="confirm-password" placeholder="Animación">
+          type="text" id="genero" name="genero" placeholder="Animación">
+      </div>
+      <div class="mb-4">
+        <label class="block text-gray-700 text-15 mb-2" for="nombreRol"><select class="w-[420px]" name="rol" id="rol">
+          <option value="Director">Director</option>
+          <option value="Actriz">Actriz</option>
+          <option value="Actor">Actor</option>
+        </select>:</label>
+        <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+          type="text" id="nombreRol" name="nombreRol" placeholder="Vicky Jenson">
       </div>
       <button
         class="w-full bg-pink text-back text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
