b61f32922dd2105d9a15d1f15bc02221a06acbcf
printMelen <alvaro.redondofgb@gmail.com>
2024-01-02 22:23:16 +0100

    Login hecho

--- app/model/Login.php
+++ app/model/Login.php
@@ -5,15 +5,14 @@ class Login
     {
         $email = Register::clean_input($_POST["email"]);
         $password = Register::clean_input($_POST["password"]);
-        $datos=[];
         try {
             $db = Conectar::conexion();
-            $sql = "SELECT correo,hash_pass,activo FROM usuariosc where correo=?";
+            $sql = "SELECT correo,hash_pass,activo,rol FROM usuariosc where correo=?";
             $resultado = $db->prepare($sql);
             $resultado->bindParam(1, $email, PDO::PARAM_STR);
             
             $resultado->execute(); 
-            var_dump($resultado->fetchAll(PDO::FETCH_ASSOC));
+            Login::dentro($resultado->fetchAll(PDO::FETCH_ASSOC));
             $resultado->closeCursor(); // opcional en MySQL, dependiendo del controlador de base de datos puede ser obligatorio
             $resultado = null; // obligado para cerrar la conexión
             $db = null; 
@@ -21,6 +20,19 @@ class Login
             echo "<br>Error: " . $e->getMessage();  
             echo "<br>Línea del error: " . $e->getLine();  
             echo "<br>Archivo del error: " . $e->getFile();
+        }
+    }
+    public static function dentro($array){
+        
+        if (count($array)!=0&&password_verify($_POST['password'],$array[0]["hash_pass"])&&$array[0]['activo']==1) {
+            if ($array[0]['rol']=='cliente') {
+                ViewController::cargarVista("indexUsuario");
+            }else{
+                ViewController::cargarVista("indexAdmin");
+
+            } 
+        }else{
+            echo "Correo o Contraseña incorrecto";
         }
     }
 }
