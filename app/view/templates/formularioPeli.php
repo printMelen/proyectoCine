384f6a1d034ae72b82c7715f487b3061a4679c02
printMelen <alvaro.redondofgb@gmail.com>
2024-01-11 12:36:12 +0100

    Generos con select

--- app/view/templates/formularioPeli.php
+++ app/view/templates/formularioPeli.php
@@ -18,16 +18,14 @@
       <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" type="number" id="edadMinima" name="edadMinima" placeholder="7">
     </div>
     <div class="mb-4">
-      <label class="block text-gray-700 text-15  mb-2" for="genero">Género:
-        <input class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" list="generos" id="genero" name="genero" placeholder="Acción">
-      </label>
-      <datalist id="generos">
+    <label class="block text-gray-700 text-15  mb-2" for="genero">Género:</label>
+    <select class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500" id="genero" name="genero">
         <?php
         foreach ($_SESSION['generos'] as $genero) {
-          echo "<option value='" . $genero['nombre']. "'>" . $genero['nombre'] . "</option>";
+          echo "<option value='" . $genero['id']. "'>" . $genero['nombre'] . "</option>";
         }
         ?>
-      </datalist>
+      </select>
     </div>
     <div class="mb-4">
       <label class="block text-gray-700 text-15 mb-2" for="nombreRol">Director:</label>
