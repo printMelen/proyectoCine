1f23e2e3cea10804628c0d4b120380f67a9aa761
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-01-24 18:01:42 +0100

    Mostrando título, descripción, director,carátula y datos aleatorios en peliGrande

--- app/view/templates/peliGrande.php
+++ app/view/templates/peliGrande.php
@@ -18,10 +18,61 @@
 
 </head>
 
-<body class="flex flex-row-reverse container bg-whiteAdmin max-w-screen-2xl justify-between mx-auto font-Poppins">
+<body class="container max-w-screen-2xl mx-auto bg-[#020510] text-white">
      <?php include("header.php"); ?>
-     
-    
+     <main class="mt-5">
+          <div class="flex items-center">
+            <div class="basis-1/3 h-[602px]">
+               <h1 class="text-3xl text-center mb-5">
+                    <?php
+                         echo $_SESSION['datosPelis'][$_GET['id']]['nombre'];
+                    ?>
+               </h1>
+               <div class="flex flex-wrap justify-around mb-5">
+                    <span class="bg-grey rounded w-[30px] text-center">
+                         <?php
+                              echo random_int(1,9).".".random_int(0,9);
+                         ?>
+                    </span>
+                    <span>
+                         <?php
+                              echo random_int(1950, date("Y"));
+                         ?>
+                    </span>
+                    <span>
+                         <?php echo $_SESSION['datosPelis'][$_GET['id']]['nombre_genero']?>
+                    </span>
+                    <span>
+                         <?php
+                              echo random_int(1,6).":".random_int(0,6)."0";
+                         ?>
+                    </span>
+                    <span>
+                         <?php
+                              echo $_SESSION['datosPelis'][$_GET['id']]['clasificacion_edad'];
+                         ?>
+                    </span>
+               </div>
+               <p>
+                    <?php
+                         echo substr($_SESSION['datosPelis'][$_GET['id']]['argumento'],0,160)."...";
+                    ?>
+               </p>
+               <div class="flex flex-col justify-around mt-5">
+               <span class="">
+                    <?php
+                    if ($_SESSION['datosPelis'][$_GET['id']]['nombre_director']!=NULL) {
+                         echo "Director: ". $_SESSION['datosPelis'][$_GET['id']]['nombre_director'];
+                    }
+                    ?>
+               </span>
+               </div>
+            </div>   
+            <div class="bg-pink basis-2/3 h-[673px]">
+               <img class="h-[100%] w-[100%]" src="app/view/images/caratula/<?php echo $_SESSION['datosPelis'][$_GET['id']]['cartel']; ?>" alt="" srcset="">
+            </div>   
+          </div>
+     </main>
      <?php include("footer.php"); ?>     
      <script src="assets/js/custom.js"></script>
 </body>
