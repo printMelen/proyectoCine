dae16646947f2e83330e75f6ccc021768fd7ef2e
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-01-25 19:55:39 +0100

    Antes de cambiar el getPelicula por id y añadiendo el obtener elenco

--- app/model/Mostrar.php
+++ app/model/Mostrar.php
@@ -2,7 +2,80 @@
 
 class Mostrar{
     public static function getPelicula($id){
-
+        $devolver = array();
+        try {
+            $db = Conectar::conexion();
+            $sql = "SELECT 
+            peliculasc.id AS id_pelicula, 
+            peliculasc.nombre AS nombre_pelicula, 
+            peliculasc.cartel AS caratula,
+            generoc.nombre AS nombre_genero, 
+            personalc.nombre AS nombre_personal, 
+            personalc.tipo AS tipo_personal
+            FROM 
+            peliculasc 
+            LEFT JOIN 
+            generoc ON peliculasc.genero_id = generoc.id 
+            LEFT JOIN 
+            peliculas_personalc ON peliculasc.id = peliculas_personalc.pelicula_id 
+            LEFT JOIN 
+            personalc ON peliculas_personalc.personal_id = personalc.id
+            where peliculasc.id=:id;
+            ";
+            $resultado = $db->prepare($sql);
+            $resultado->bindParam(":id", $id);
+            $resultado->execute(); 
+            $devolver=$resultado->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($devolver as &$pelicula) {
+                $pelicula['caratula'] = CMostrar::getRuta() . $pelicula['caratula'];
+                // $pelicula['elenco'];
+            }
+            $resultado->closeCursor();
+            $resultado = null;
+            $db = null; 
+        } catch (PDOException $e) {
+            echo "<br>Error: " . $e->getMessage();  
+            echo "<br>Línea del error: " . $e->getLine();  
+            echo "<br>Archivo del error: " . $e->getFile();
+        }
+        return $devolver;
+    }
+    public static function obtenerElenco($i){
+        $devolver = array();
+        try {
+            $db = Conectar::conexion();
+            $sql = "SELECT  
+            personalc.id AS id_personal, 
+            personalc.nombre AS nombre_personal, 
+            personalc.imagen AS imagen_personal, 
+            personalc.tipo AS tipo_personal
+            FROM 
+            peliculasc 
+            LEFT JOIN 
+            generoc ON peliculasc.genero_id = generoc.id 
+            LEFT JOIN 
+            peliculas_personalc ON peliculasc.id = peliculas_personalc.pelicula_id 
+            LEFT JOIN 
+            personalc ON peliculas_personalc.personal_id = personalc.id
+            where peliculasc.id=:id;
+            ";
+            $resultado = $db->prepare($sql);
+            $resultado->bindParam(":id", $id);
+            $resultado->execute(); 
+            $devolver=$resultado->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($devolver as &$pelicula) {
+                $pelicula['caratula'] = CMostrar::getRuta() . $pelicula['caratula'];
+                // $pelicula['elenco'];
+            }
+            $resultado->closeCursor();
+            $resultado = null;
+            $db = null; 
+        } catch (PDOException $e) {
+            echo "<br>Error: " . $e->getMessage();  
+            echo "<br>Línea del error: " . $e->getLine();  
+            echo "<br>Archivo del error: " . $e->getFile();
+        }
+        return $devolver;
     }
     public static function getPeliculas(){
         $devolver = array();
