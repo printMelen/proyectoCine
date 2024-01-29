61563a28011d7797dbcdfa23e991c5ff74078962
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-01-29 17:40:14 +0100

    Métodos para sacar la fecha sin cambiar formato

--- app/controller/MoviePreviewController.php
+++ app/controller/MoviePreviewController.php
@@ -4,6 +4,10 @@ class MoviePreviewController
     public static function inicio()
     {
         if (isset($_GET['id'])) {
+            $_SESSION['fechas']=Fecha::formatearFecha(Fecha::sacarFecha());
+            echo "<pre>";
+            var_dump($_SESSION['fechas']);
+            echo "</pre>";
             ViewController::cargarVista("peliGrande");
         }
     }
---
+++ app/model/Fecha.php
@@ -0,0 +1,46 @@
+<?php
+class Fecha{
+    public static function sacarFecha() {
+        $devolver = array();
+        try {
+            $db = Conectar::conexion();
+            $sql = "SELECT 
+                peliculasc.id,
+                sesionesc.pelicula_id,
+                sesionesc.fecha
+                FROM 
+                peliculasc 
+                LEFT JOIN 
+                sesionesc ON peliculasc.id = sesionesc.pelicula_id 
+                GROUP BY 
+                sesionesc.id;
+            ";
+            $resultado = $db->prepare($sql);
+            $resultado->execute(); 
+            $devolver=$resultado->fetchAll(PDO::FETCH_ASSOC);
+            $resultado->closeCursor();
+            $resultado = null;
+            $db = null; 
+        } catch (PDOException $e) {
+            echo "<br>Error: " . $e->getMessage();  
+            echo "<br>Línea del error: " . $e->getLine();  
+            echo "<br>Archivo del error: " . $e->getFile();
+        }
+        return $devolver;
+    }
+    public static function formatearFecha($fechas){
+        $final=array();
+        foreach ($fechas as $key => $fecha) {
+            foreach ($final as $key => $fin) {
+                if ($fin['fecha']==$fecha['fecha']) {
+                    unset($final[$key]);
+                }
+    
+            }
+            if ($fecha['id']==$_GET['id']+1) {
+                array_push($final,$fecha);
+            }
+        }
+        return $final;
+    }
+}
