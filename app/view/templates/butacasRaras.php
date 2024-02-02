df8f823e621061515289b99da81b94d8fca6320e
Alvaro Redondo Rodriguez <alvaro.redrod.1@educa.jcyl.es>
2024-02-02 19:12:17 +0100

    Butacas raras con tablas

--- app/view/templates/butacasRaras.php
+++ app/view/templates/butacasRaras.php
@@ -1,20 +1,3 @@
-<div class="flex flex-col w-[1040px] text-back">
-    <div class="todo flex justify-between">
-        <div class="butacas flex flex-wrap w-[278px]">
-        
-        </div>
-        <div class="butacas flex flex-wrap w-[48px]">
-        
-        </div>
-        <div class="butacas flex flex-wrap w-[342px]">
-            
-        </div>
-        <div class="butacas flex flex-wrap justify-end w-[225px]">
-            
-        </div>
-    </div>
-        <div class="flex justify-between items-center" id="fila">
+<table class="text-back">
     
-        </div>
-        
-</div>
+</table>
