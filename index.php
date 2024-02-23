d9b8f604d478bd73aed8fff5fde2f61c5396aad8
printMelen <alvaro.redondofgb@gmail.com>
2024-02-23 01:07:18 +0100

    Para el servidor

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-// const URL = "http://143.47.43.204:8080/alvaro/proyectoCine/api/v1/cine";
-// const URLPDF = "http://143.47.43.204:8080/alvaro/proyectoCine/pdf/";
-// const URLCSS = "http://143.47.43.204:8080/alvaro/proyectoCine/app/view/css/dist/output.css";
-// const URLIMG ="http://143.47.43.204:8080/alvaro/proyectoCine/app/view/images/";
-// const URLQR ="http://143.47.43.204:8080/alvaro/proyectoCine/qr/qr_generados/";
-const URL = "http://localhost/dwes/proyectoCine/api/v1/cine";
-const URLPDF = "http://localhost/dwes/proyectoCine/pdf/";
-const URLCSS = "http://localhost/dwes/proyectoCine/app/view/css/dist/output.css";
-const URLIMG ="http://localhost/dwes/proyectoCine/app/view/images/";
-const URLQR ="http://localhost/dwes/proyectoCine/qr/qr_generados/";
+const URL = "http://143.47.43.204:8080/alvaro/proyectoCine/api/v1/cine";
+const URLPDF = "http://143.47.43.204:8080/alvaro/proyectoCine/pdf/";
+const URLCSS = "http://143.47.43.204:8080/alvaro/proyectoCine/app/view/css/dist/output.css";
+const URLIMG ="http://143.47.43.204:8080/alvaro/proyectoCine/app/view/images/";
+const URLQR ="http://143.47.43.204:8080/alvaro/proyectoCine/qr/qr_generados/";
+// const URL = "http://localhost/dwes/proyectoCine/api/v1/cine";
+// const URLPDF = "http://localhost/dwes/proyectoCine/pdf/";
+// const URLCSS = "http://localhost/dwes/proyectoCine/app/view/css/dist/output.css";
+// const URLIMG ="http://localhost/dwes/proyectoCine/app/view/images/";
+// const URLQR ="http://localhost/dwes/proyectoCine/qr/qr_generados/";
 require_once "autoload.php";
 include "app/controller/enrutador.php";
 // const URL = "http://143.47.43.204:8080/alvaro/proyectoCine/api/v1/cine";
